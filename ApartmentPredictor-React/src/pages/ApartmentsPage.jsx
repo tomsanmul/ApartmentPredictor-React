b7ccc9c0752f7679be198a931ce98c6226cf3489
tomsanmul <tomsan@gmail.com>
2026-03-13 08:56:50 +0100

    Canvis

--- ApartmentPredictor-React/src/pages/ApartmentsPage.jsx
+++ ApartmentPredictor-React/src/pages/ApartmentsPage.jsx
@@ -2,5 +2,8 @@ import { Typography } from "@mui/material";
 import ApartmentList from "./apartment/ApartmentList"; 
 
 export default function ApartmentsPage() { 
-  return <Typography variant="h4"><ApartmentList /></Typography>; 
+  return <Typography variant="h4">
+    <ApartmentList />
+    </Typography>; 
+
 }
