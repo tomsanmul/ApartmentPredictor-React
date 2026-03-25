375bec2df02435187d968f5c691520fc0bdcfca1
tomsanmul <tomsan@gmail.com>
2026-03-25 16:56:51 +0100

    Filter CSS

--- ApartmentPredictor-React/src/apartment/ApartmentsFilter.jsx
+++ ApartmentPredictor-React/src/apartment/ApartmentsFilter.jsx
@@ -1,33 +1,119 @@
-import React from "react";
+import React, { useState } from "react";
 import "../index.css";
 
-export default function ApartmentsFilter() {
+export default function ApartmentsFilter({ onFilter }) {
+  const [filters, setFilters] = useState({
+    minPrice: "",
+    maxPrice: "",
+    minArea: "",
+    maxArea: "",
+    bedrooms: "",
+    bathrooms: "",
+    stories: "",
+    parking: "",
+    mainroad: "no",
+    guestroom: "no",
+    basement: "no",
+    hotwaterheating: "no",
+    airconditioning: "no",
+    prefarea: "no",
+    furnishingstatus: "none"
+  });
+
+  const handleChange = (e) => {
+    const { name, value, type, checked } = e.target;
+
+    if (type === "checkbox") {
+      setFilters((prev) => ({
+        ...prev,
+        [name]: checked ? "yes" : "no"
+      }));
+    } else {
+      setFilters((prev) => ({
+        ...prev,
+        [name]: value
+      }));
+    }
+  };
+
+  const handleSubmit = () => {
+    onFilter(filters);
+  };
+
   return (
     <div className="filter-container">
       <h2 className="filter-title">Filter Apartments</h2>
 
-      <div className="filter-field">
-        <label>Price min:</label>
-        <input type="text" placeholder="Ej: 1000" />
+      {/* FILA 1 */}
+      <div className="filter-row">
+        <div className="filter-field">
+          <label>Min Price</label>
+          <input type="number" name="minPrice" value={filters.minPrice} onChange={handleChange} />
+        </div>
+
+        <div className="filter-field">
+          <label>Max Price</label>
+          <input type="number" name="maxPrice" value={filters.maxPrice} onChange={handleChange} />
+        </div>
+
+        <div className="filter-field">
+          <label>Min Area</label>
+          <input type="number" name="minArea" value={filters.minArea} onChange={handleChange} />
+        </div>
+
+        <div className="filter-field">
+          <label>Max Area</label>
+          <input type="number" name="maxArea" value={filters.maxArea} onChange={handleChange} />
+        </div>
+
+        <div className="filter-field">
+          <label>Bedrooms</label>
+          <input type="number" name="bedrooms" value={filters.bedrooms} onChange={handleChange} />
+        </div>
+
+        <div className="filter-field">
+          <label>Bathrooms</label>
+          <input type="number" name="bathrooms" value={filters.bathrooms} onChange={handleChange} />
+        </div>
       </div>
 
-      <div className="filter-field">
-        <label>Parkings:</label>
-        <input type="number" placeholder="Ej: 1" />
+      {/* FILA 2 */}
+      <div className="filter-row">
+        <div className="filter-field">
+          <label>Stories</label>
+          <input type="number" name="stories" value={filters.stories} onChange={handleChange} />
+        </div>
+
+        <div className="filter-field">
+          <label>Parking</label>
+          <input type="number" name="parking" value={filters.parking} onChange={handleChange} />
+        </div>
+
+        {/* CHECKBOXES */}
+        <div className="checkbox-group">
+          <label>Main Road <input type="checkbox" name="mainroad" onChange={handleChange} /></label>
+          <label>Guest Room <input type="checkbox" name="guestroom" onChange={handleChange} /></label>
+          <label>Basement <input type="checkbox" name="basement" onChange={handleChange} /></label>
+          <label>Hot Water <input type="checkbox" name="hotwaterheating" onChange={handleChange} /></label>
+          <label>Air Conditioning <input type="checkbox" name="airconditioning" onChange={handleChange} /></label>
+          <label>Preferred Area <input type="checkbox" name="prefarea" onChange={handleChange} /></label>
+        </div>
+
+        {/* SELECT */}
+        <div className="filter-field furnishing">
+          <label>Furnishing</label>
+          <select name="furnishingstatus" value={filters.furnishingstatus} onChange={handleChange}>
+            <option value="none">none</option>
+            <option value="furnished">furnished</option>
+            <option value="unfurnished">unfurnished</option>
+            <option value="semi-furnished">semi-furnished</option>
+          </select>
+        </div>
       </div>
 
-      <div className="filter-field">
-        <label>Reviews min:</label>
-        <input type="number" placeholder="Ej: 10" />
-      </div>
-
-      <div className="filter-field">
-        <label>Schools min:</label>
-        <input type="number" placeholder="Ej: 2" />
-      </div>
-
-      <button className="filter-button">FILTER</button>
+      <button className="filter-button" onClick={handleSubmit}>
+        FILTER
+      </button>
     </div>
   );
-};
-
+}
