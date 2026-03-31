dccbbef502847554c7b1f59be975a6b8d366ce1c
tomsanmul <tomsan@gmail.com>
2026-03-31 11:55:25 +0200

    Canvis

---
+++ ApartmentPredictor-React/src/pages/schoolsPage.jsx
@@ -0,0 +1,11 @@
+import SchoolMap from "../school/SchoolMap";
+
+const SchoolsPage = () => {
+    return (
+        <div style={{ width: '100%', height: '100%' }}>
+            <SchoolMap />
+        </div>
+    );
+};
+
+export default SchoolsPage;
