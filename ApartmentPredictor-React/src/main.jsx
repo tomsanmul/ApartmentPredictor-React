7cb9487afde0d036b42769732d14d771aaf7db60
tomsanmul <tomsan@gmail.com>
2026-04-10 13:31:40 +0200

    Feat: Cognito

--- ApartmentPredictor-React/src/main.jsx
+++ ApartmentPredictor-React/src/main.jsx
@@ -1,5 +1,18 @@
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { AuthProvider } from "react-oidc-context";
 
-createRoot(document.getElementById('root')).render(<App />)
+const cognitoAuthConfig = {
+  authority: "https://cognito-idp.eu-central-1.amazonaws.com/eu-central-1_RxivNkxWJ",
+  client_id: "3oq8aq4m5avt94d0qho82gmk5u",
+  redirect_uri: "http://localhost:5173/apartments",
+  response_type: "code",
+  scope: "openid email phone",
+};
+
+createRoot(document.getElementById("root")).render(
+  <AuthProvider {...cognitoAuthConfig}>
+    <App />
+  </AuthProvider>
+);
